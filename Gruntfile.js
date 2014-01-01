72e89ea58bc4b2324a53d8e8f30218b481811c13
Lars Thorup <lars@zealake.com>
2014-01-01 15:44:36 -0800

    grunt lint

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+/*global module*/
+/*jshint camelcase:false*/ // because of gruntConfig.qunit_junit
+module.exports = function (grunt) {
+    'use strict';
+
+    var gruntConfig = {
+        pkg: grunt.file.readJSON('package.json')
+    };
+
+    // convenience
+    grunt.registerTask('default', ['lint']);
+    grunt.registerTask('all', ['lint']);
+
+    // continuous integration
+    grunt.registerTask('ci', ['lint']);
+
+
+    // lint
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    gruntConfig.jshint = {
+        options: { bitwise: true, camelcase: true, curly: true, eqeqeq: true, forin: true, immed: true,
+            indent: 4, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, plusplus: true,
+            quotmark: true, regexp: true, undef: true, unused: true, strict: true, trailing: true,
+            maxparams: 3, maxdepth: 2, maxstatements: 50},
+        all: [
+            'Gruntfile.js',
+            'src/js/**/*.js'
+        ]
+    };
+    grunt.registerTask('lint', 'jshint');
+
+
+    // grunt
+    grunt.initConfig(gruntConfig);
+};
